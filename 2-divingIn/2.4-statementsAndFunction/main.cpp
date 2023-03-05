906f57d04144f39dad6b11e867ce3371d5e9d943
Dayanand Singh <imlucifer.dev@gmail.com>
2023-03-05 20:45:39 +0530

    Sun 05 March,2023|ch 02 Driving in|2.4 statement and funtion complete|todo 2.5 Input Output|

--- 2-divingIn/2.4-statementsAndFunction/main.cpp
+++ 2-divingIn/2.4-statementsAndFunction/main.cpp
@@ -6,10 +6,35 @@
 
 #include<iostream>
 
+//* addme Defined
+int addme(int fpar ,int spar){
+    int sum = fpar + spar;
+    return sum;
+}
+
 int main(int argc, char const *argv[])
 {
     std::cout<<" ==================================== Start Here ============================================"
     <<std::endl;
+
+    int first_number{3};
+    int secon_number{7};
+
+    std::cout<<"First Number : "<<first_number<<std::endl;// 3
+    std::cout <<"Second Number : "<<secon_number<<std::endl;// 7
+
+    int sum = first_number +secon_number;
+
+    std::cout<<"Sum : "<<sum<<std::endl;// 10
+
+    // TODO: calling addme Function
+    sum = addme(25,7);
+    std::cout<<"Sum By addme Function: "<<sum<<std::endl; // 32
+
+    sum = addme(52,27);
+    std::cout<<"Sum By addme Function: "<<sum<<std::endl; // 79
+
+    std::cout<<"ADD 30 + 77 : "<<addme(30,77)<<std::endl; // 107
 
     
     std::cout<<" ==================================== Finish Here ==========================================="
