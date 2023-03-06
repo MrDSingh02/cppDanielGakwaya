fe63351613e6c569b704f63ac1a7c83986b50a89
Dayanand Singh <imlucifer.dev@gmail.com>
2023-03-07 04:39:46 +0530

    3.3 integer lesson start

---
+++ 3-VariablesAndDataTypes/3.3-IntegerTypes-IntegersAndDecimals/main.cpp
@@ -0,0 +1,23 @@
+// ! 3.3 Integers Type : Integer and Decimals
+// ? Time :| 3h : 21m : 53s |
+// * Code :||
+// Todo: | g++ -std=c++20 -o gccBuild main.cpp |
+// Todo: | clang++ -std=c++20 -o clangBuild main.cpp |
+// // 
+/*
+
+*/
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<" 3.3 Integers Type : Integer and Decimals Introduction "
+    <<std::endl;
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "
+    <<std::endl;
+
+    
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "
+    <<std::endl;
+    return 0;
+}
---
+++ 3-VariablesAndDataTypes/3.3-IntegerTypes-IntegersAndDecimals/readme.md
@@ -0,0 +1,10 @@
+# CH 3 Variables And Data Types
+
+# 3.3 Integers Type : Integer and Decimals
+
+Time : | 3h : 21m : 53s |
+Code : | |
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+
+---
