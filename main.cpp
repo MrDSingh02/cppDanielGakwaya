adba931e5924cf16d2a028dd8a55caa799387237
Dayanand Singh <imlucifer.dev@gmail.com>
2023-03-08 06:52:49 +0530

    some basic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // ! Template
 // ? Time :||
-// * Code :||
+// * Code :| 4h : 03m : 44s |
 // Todo: | g++ -std=c++20 -o build.gcc main.cpp |
 // Todo: | clang++ -std=c++20 -o build.clang main.cpp |
 // // 
@@ -10,7 +10,7 @@
 # 3.2 Number Systems
 
 Time : | 3h : 05m : 06s |
-Code : | |
+Code : | 4h : 03m : 44s |
 * [main.cpp](./main.cpp)
 * [Home](/README.md)
 
@@ -21,13 +21,10 @@ Code : | |
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<"---------------- 3.1 Variables And Data Types Introduction ---------------------------"
-    <<std::endl;
-    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "
-    <<std::endl;
+    std::cout<<" 3.1 Variables And Data Types Introduction "<<std::endl;
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
 
     
-    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "
-    <<std::endl;
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
     return 0;
 }
