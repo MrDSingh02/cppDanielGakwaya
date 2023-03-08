84991564e5adfcbf74f8270690ff205a87dca9e1
Dayanand Singh <imlucifer.dev@gmail.com>
2023-03-08 06:11:52 +0530

    3.5 Fractional Number Start

---
+++ 3-VariablesAndDataTypes/3.5-FractionalNumbers/main.cpp
@@ -0,0 +1,22 @@
+//! 3.5 Fractional Numbers
+// ? Time :| 3h : 54m : 02s |
+// * Code :||
+// Todo: | g++ -std=c++20 -o build.gcc main.cpp |
+// Todo: | clang++ -std=c++20 -o build.clang main.cpp |
+// // 
+/*
+
+*/
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"3.5 Fractional Numbers"<<std::endl;
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "
+    <<std::endl;
+
+    
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "
+    <<std::endl;
+    return 0;
+}
---
+++ 3-VariablesAndDataTypes/3.5-FractionalNumbers/readme.md
@@ -0,0 +1,10 @@
+# CH 3 Variables And Data Types
+
+# 3.5 Fractional Numbers
+
+Time : | 3h : 54m : 02s |
+Code : | |
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+
+---
