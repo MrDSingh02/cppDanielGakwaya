e0e4fb1d321f91cb26ab46904d07d29b08efb392
Dayanand Singh <imlucifer.dev@gmail.com>
2023-03-10 07:24:11 +0530

    3.9 Assignments Lesson Start

--- 3-VariablesAndDataTypes/3.9-Assignments/main.cpp
+++ 3-VariablesAndDataTypes/3.9-Assignments/main.cpp
@@ -1,24 +1,11 @@
 // ! 3.9 Assignments
-// ? Start Time :           | 00H : 00Min : 00 Sec |
+// ? Start Time :           | 04H : 38Min : 08 Sec |
 //*  Code Time  :           | 00H : 00Min : 00 Sec |
 //*  Left Time  :           | 00H : 00Min : 00 Sec |
 // Todo: | g++ -std=c++20 -o build.gcc main.cpp |
 // Todo: | clang++ -std=c++20 -o build.clang main.cpp |
 // del 
 /*
-# CH 3 Variables And Data Types
-
-# 3.9 Assignments
-
-||Time (00H : 00Min : 00 Sec)|
-|-|-|
- |Lesson Start           | 00H : 00Min : 00 Sec |  
- |Code Start             | 00H : 00Min : 00 Sec |  
- |Next Lesson            | 00H : 00Min : 00 Sec | 
-* [main.cpp](./main.cpp)
-* [Home](/README.md)
-
----
 
 */
 #include<iostream>
---
+++ 3-VariablesAndDataTypes/3.9-Assignments/readme.md
@@ -0,0 +1,13 @@
+# CH 3 Variables And Data Types
+
+# 3.9 Assignments
+
+||Time (00H : 00Min : 00 Sec)|
+|-|-|
+ |Lesson Start           | 04H : 38Min : 07 Sec |  
+ |Code Start             | 00H : 00Min : 00 Sec |  
+ |Next Lesson            | 00H : 00Min : 00 Sec | 
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+
+---
