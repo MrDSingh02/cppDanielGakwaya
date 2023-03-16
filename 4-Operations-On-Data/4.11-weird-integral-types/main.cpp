98a12b1871137431b2b267b3d25279bc582558b3
Dayanand Singh <imlucifer.dev@gmail.com>
2023-03-17 04:29:36 +0530

    basic setup done

---
+++ 4-Operations-On-Data/4.11-weird-integral-types/main.cpp
@@ -0,0 +1,22 @@
+
+// ! 4.11 Weird Integral types
+// ? |Lesson Start           | 06H : 54Min : 25 Sec |
+//*  |Code Start             | 06H : 57Min : 03 Sec |
+//*  |Next Lesson            | 00H : 00Min : 00 Sec |
+// >>: | g++ -std=c++20 -o build.gcc main.cpp |
+// Todo: | clang++ -std=c++20 -o build.clang main.cpp |
+// || 
+/*
+
+*/
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<" 4.11 Weird Integral types "<<std::endl;
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+
+    
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+    return 0;
+}
---
+++ 4-Operations-On-Data/4.11-weird-integral-types/readme.md
@@ -0,0 +1,13 @@
+# Chapter 04 Operations On Data
+
+## 4.11 Weird Integral types
+
+||Time (00H : 00Min : 00 Sec)|
+|-|-|
+ |Lesson Start           | 06H : 54Min : 25 Sec |  
+ |Code Start             | 06H : 57Min : 03 Sec |  
+ |Next Lesson            | 00H : 00Min : 00 Sec | 
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+
+---
