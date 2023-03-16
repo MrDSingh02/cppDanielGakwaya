c426a29fb7423d4cf618961339d3f558d688e9c9
Dayanand Singh <imlucifer.dev@gmail.com>
2023-03-16 06:39:28 +0530

    4.10 math function basic setup done

---
+++ 4-Operations-On-Data/4.10-math-function/main.cpp
@@ -0,0 +1,21 @@
+// ! 4.10 Math function
+// ? |Lesson Start           | 06H : 41Min : 12 Sec |
+//*  |Code Start             | 00H : 00Min : 00 Sec |
+//*  |Next Lesson            | 00H : 00Min : 00 Sec |
+// Todo: | g++ -std=c++20 -o build.gcc main.cpp |
+// Todo: | clang++ -std=c++20 -o build.clang main.cpp |
+// del 
+/*
+
+*/
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<" 4.10 Math function "<<std::endl;
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+
+    
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+    return 0;
+}
---
+++ 4-Operations-On-Data/4.10-math-function/readme.md
@@ -0,0 +1,13 @@
+# Chapter 04 Operations On Data
+
+## 4.10 Math function
+
+||Time (00H : 00Min : 00 Sec)|
+|-|-|
+ |Lesson Start           | 06H : 41Min : 12 Sec |  
+ |Code Start             | 00H : 00Min : 00 Sec |  
+ |Next Lesson            | 00H : 00Min : 00 Sec | 
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+
+---
