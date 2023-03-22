39a12357c5e9285d3fd0a97de4ff73769b46c324
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-03-22 08:11:52 +0530

    6.2 For loop finishe

--- 6-loops/6.2-for-loop/main.cpp
+++ 6-loops/6.2-for-loop/main.cpp
@@ -1,7 +1,7 @@
 // Chapter 6: Loops
 // ! 6.1 Loops Introduction
 // ? |Lesson Start           | 07H : 55Min : 20 Sec |
-//*  |Code Start             | 00H : 00Min : 00 Sec |
+//*  |Code Start             | 08H : 06Min : 32 Sec |
 //*  |Next Lesson            | 00H : 00Min : 00 Sec |
 // >>: | g++ -std=c++20 -o build.gcc main.cpp |
 // ||: | clang++ -std=c++20 -o build.clang main.cpp |
@@ -15,8 +15,72 @@ int main(int argc, char const *argv[])
 {
     std::cout<<" 6.1 Loops Introduction "<<std::endl;
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+/* 
+    // >> Print I Love C++ 10 Times
+    std::cout << "I Love C++ " << std::endl;
+    std::cout << "I Love C++ " << std::endl;
+    std::cout << "I Love C++ " << std::endl;
+    std::cout << "I Love C++ " << std::endl;
+    std::cout << "I Love C++ " << std::endl;
+    std::cout << "I Love C++ " << std::endl;
+    std::cout << "I Love C++ " << std::endl;
+    std::cout << "I Love C++ " << std::endl;
+    std::cout << "I Love C++ " << std::endl;
+    std::cout << "I Love C++ " << std::endl;
+     */
 
-    
+    // >> for loop : the good way
+/* 
+    for (unsigned int i{0}, j{10}; i < j; ++i){
+        //  whatever we want the loop to run
+        std::cout << "I Love C++20 " << i << " Times." <<std::endl;
+    }
+    std::cout << "Loop Done" << std::endl;
+ */
+
+    // >> Use 'size_t' : a representation of some unsigned int for positive number [sizes]
+/* 
+    for(size_t i{0}, j{10}; i < j; ++i){
+        std::cout << i << ": I Love C++20" << std::endl;
+    }
+    std::cout << "For Loop Done" << std::endl;
+*/  
+/* 
+    //>> sizeof(size_t)
+    std::cout << "sizeof(size_t) : " << sizeof(size_t) << std::endl; //8 bytes in gcc
+ */
+/* 
+    //>> Scope of the iterator
+    for(size_t i{0},j{10}; i < j; ++i){
+        std::cout << i << " : I love C++20" << std::endl;
+    }
+    // std::cout << "Loop Done!" << j << std::endl; //! compile error: j is not in scope using loop 
+ */
+/* 
+    //>> Iterator declared outside the loop
+    size_t i {0}, j{10};
+    for (i, j;i < j; ++i){
+        std::cout << i << ": I love c++20" << std::endl;
+    }
+    std::cout << "Loop Done!" << std::endl;
+    std::cout << "i : " << i << ", j : " << j << std::endl;
+ */
+/* 
+    //>> Leave out the iterator declaration part
+    size_t i {0}, j{10};
+    for (;i < j; ++i){
+        std::cout << i << ": I love c++20" << std::endl;
+    }
+    std::cout << "Loop Done!" << std::endl;
+    std::cout << "i : " << i << ", j : " << j << std::endl;
+ */
+    //>> Don't hard code values : BAD!
+
+    const size_t COUNT{100};
+    for(size_t i{0}; i < COUNT; ++i ){
+        std::cout << i << ": I Love c++20 " << std::endl;
+    }
+
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
     return 0;
 }
--- 6-loops/6.2-for-loop/readme.md
+++ 6-loops/6.2-for-loop/readme.md
@@ -5,7 +5,7 @@
 ||Time (00H : 00Min : 00 Sec)|
 |-|-|
  |Lesson Start           | 07H : 55Min : 20 Sec |  
- |Code Start             | 00H : 00Min : 00 Sec |  
+ |Code Start             | 08H : 06Min : 32 Sec |  
  |Next Lesson            | 00H : 00Min : 00 Sec | 
 * [main.cpp](./main.cpp)
 * [Home](/README.md)
