3e51fb46d7bf226cf8ffb6071f967e154dce51c5
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-03-22 06:11:16 +0530

    6.2 For Loop Fixes #39 basic setup

--- 6-loops/6.2-for-loop/main.cpp
+++ 6-loops/6.2-for-loop/main.cpp
@@ -1,26 +1,12 @@
 // Chapter 6: Loops
 // ! 6.1 Loops Introduction
-// ? |Lesson Start           | 00H : 00Min : 00 Sec |
+// ? |Lesson Start           | 07H : 55Min : 20 Sec |
 //*  |Code Start             | 00H : 00Min : 00 Sec |
 //*  |Next Lesson            | 00H : 00Min : 00 Sec |
 // >>: | g++ -std=c++20 -o build.gcc main.cpp |
 // ||: | clang++ -std=c++20 -o build.clang main.cpp |
 // del 
 /*
-# Chapter 6: Loops
-
-## 6.1 Loops Introduction
-
-||Time (00H : 00Min : 00 Sec)|
-|-|-|
- |Lesson Start           | 00H : 00Min : 00 Sec |  
- |Code Start             | 00H : 00Min : 00 Sec |  
- |Next Lesson            | 00H : 00Min : 00 Sec | 
-* [main.cpp](./main.cpp)
-* [Home](/README.md)
-* [pdf]()
-
----
 
 */
 #include<iostream>
