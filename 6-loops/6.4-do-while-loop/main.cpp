2bd0908490149f60fdaa0bc54eb4d97a38f50375
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-03-23 09:03:03 +0530

    6.4 do while loop and chapter 6 loop finished

--- 6-loops/6.4-do-while-loop/main.cpp
+++ 6-loops/6.4-do-while-loop/main.cpp
@@ -1,34 +1,44 @@
 // Chapter 6: Loops
-// ! 6.1 Loops Introduction
+// ! 6.4 Do while Loop
 // ? |Lesson Start           | 08H : 36Min : 55 Sec |
-//*  |Code Start             | 00H : 00Min : 00 Sec |
-//*  |Next Lesson            | 00H : 00Min : 00 Sec |
+//*  |Code Start             | 08H : 38Min : 46 Sec |
+//*  |Next Lesson            | 08H : 47Min : 13 Sec |
 // >>: | g++ -std=c++20 -o build.gcc main.cpp |
 // ||: | clang++ -std=c++20 -o build.clang main.cpp |
 // del 
 /*
-# Chapter 6: Loops
-
-## 6.1 Loops Introduction
-
-||Time (00H : 00Min : 00 Sec)|
-|-|-|
- |Lesson Start           | 08H : 36Min : 55 Sec |  
- |Code Start             | 00H : 00Min : 00 Sec |  
- |Next Lesson            | 00H : 00Min : 00 Sec | 
-* [main.cpp](./main.cpp)
-* [Home](/README.md)
-* [pdf]()
-
----
 
 */
 #include<iostream>
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<" 6.1 Loops Introduction "<<std::endl;
+    std::cout<<" 6.4 Do while Loop "<<std::endl;
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+
+    //>> Print I love c++20 10times
+    /* std::cout << "I love c++20 " << std::endl;
+    std::cout << "I love c++20 " << std::endl;
+    std::cout << "I love c++20 " << std::endl;
+    std::cout << "I love c++20 " << std::endl;
+    std::cout << "I love c++20 " << std::endl;
+    std::cout << "I love c++20 " << std::endl;
+    std::cout << "I love c++20 " << std::endl;
+    std::cout << "I love c++20 " << std::endl;
+    std::cout << "I love c++20 " << std::endl;
+    std::cout << "I love c++20 " << std::endl; */
+
+    const unsigned int COUNT{10};
+    size_t i{0}; // iterator declaration
+
+    do{
+        std::cout << i << " : I love c++20." << std::endl;
+        ++i; // Incrementation
+    }while (i < COUNT);
+
+    std::cout << i << "Loop Done!" << std::endl;
+    
+    
 
     
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
---
+++ 7-Arrays/7.1-introduction/readme.md
@@ -0,0 +1,14 @@
+# Chapter 7: Arrays
+
+## 7.1 Introduction to Arrays
+
+||Time (00H : 00Min : 00 Sec)|
+|-|-|
+ |Lesson Start           | 08H : 47Min : 13 Sec |  
+ |Code Start             | 00H : 00Min : 00 Sec |  
+ |Next Lesson            | 00H : 00Min : 00 Sec | 
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+* [pdf]()
+
+---
