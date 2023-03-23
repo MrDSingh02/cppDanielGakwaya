9acf6fc8258eedc9905e4936e6d5d2b2c4c3579e
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-03-23 08:31:17 +0530

    6.3 while loop finished

--- 6-loops/6.3-while-loop/main.cpp
+++ 6-loops/6.3-while-loop/main.cpp
@@ -1,26 +1,12 @@
 // Chapter 6: Loops
 // ! 6.3 While Loop
 // ? |Lesson Start           | 08H : 25Min : 23 Sec |
-//*  |Code Start             | 00H : 00Min : 00 Sec |
-//*  |Next Lesson            | 00H : 00Min : 00 Sec |
+//*  |Code Start             | 08H : 29Min : 07 Sec |
+//*  |Next Lesson            | 08H : 36Min : 55 Sec |
 // >>: | g++ -std=c++20 -o build.gcc main.cpp |
 // ||: | clang++ -std=c++20 -o build.clang main.cpp |
 // del 
 /*
-# Chapter 6: Loops
-
-## 6.3 While Loop
-
-||Time (00H : 00Min : 00 Sec)|
-|-|-|
- |Lesson Start           | 08H : 25Min : 23 Sec |  
- |Code Start             | 00H : 00Min : 00 Sec |  
- |Next Lesson            | 00H : 00Min : 00 Sec | 
-* [main.cpp](./main.cpp)
-* [Home](/README.md)
-* [pdf]()
-
----
 
 */
 #include<iostream>
@@ -30,6 +16,26 @@ int main(int argc, char const *argv[])
     std::cout<<" 6.3 While Loop "<<std::endl;
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
 
+    // >> Print I Love c++ 10 times
+    /* std::cout << "I Love C++20" << std::endl;
+    std::cout << "I Love C++20" << std::endl;
+    std::cout << "I Love C++20" << std::endl;
+    std::cout << "I Love C++20" << std::endl;
+    std::cout << "I Love C++20" << std::endl;
+    std::cout << "I Love C++20" << std::endl;
+    std::cout << "I Love C++20" << std::endl;
+    std::cout << "I Love C++20" << std::endl;
+    std::cout << "I Love C++20" << std::endl;
+    std::cout << "I Love C++20" << std::endl; */
+
+    const size_t COUNT {100};
+    size_t i {0};//* iteration declaration
+
+    while (i < COUNT){ //* test
+        std::cout << i << " : I Love C++20" << std::endl;
+        ++i; // Incrementation
+    }
+    std::cout << " LOOP DONE!" << std::endl;
     
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
     return 0;
---
+++ 6-loops/6.3-while-loop/readme.md
@@ -0,0 +1,14 @@
+# Chapter 6: Loops
+
+## 6.3 While Loop
+
+||Time (00H : 00Min : 00 Sec)|
+|-|-|
+ |Lesson Start           | 08H : 25Min : 23 Sec |  
+ |Code Start             | 08H : 29Min : 07 Sec |  
+ |Next Lesson            | 08H : 36Min : 55 Sec | 
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+* [pdf]()
+
+---
