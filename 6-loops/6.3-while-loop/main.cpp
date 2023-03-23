391c965c2c9e74c813ece9e823ff2d5c18dcd9c3
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-03-23 08:09:51 +0530

    6.3 while Loop Fixes #40 basic setup done

---
+++ 6-loops/6.3-while-loop/main.cpp
@@ -0,0 +1,36 @@
+// Chapter 6: Loops
+// ! 6.3 While Loop
+// ? |Lesson Start           | 08H : 25Min : 23 Sec |
+//*  |Code Start             | 00H : 00Min : 00 Sec |
+//*  |Next Lesson            | 00H : 00Min : 00 Sec |
+// >>: | g++ -std=c++20 -o build.gcc main.cpp |
+// ||: | clang++ -std=c++20 -o build.clang main.cpp |
+// del 
+/*
+# Chapter 6: Loops
+
+## 6.3 While Loop
+
+||Time (00H : 00Min : 00 Sec)|
+|-|-|
+ |Lesson Start           | 08H : 25Min : 23 Sec |  
+ |Code Start             | 00H : 00Min : 00 Sec |  
+ |Next Lesson            | 00H : 00Min : 00 Sec | 
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+* [pdf]()
+
+---
+
+*/
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<" 6.3 While Loop "<<std::endl;
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+
+    
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+    return 0;
+}
