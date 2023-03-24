ed8740220de03cef3d1ad2a015ae5578bc547190
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-03-24 10:24:02 +0530

    code time start

--- 7-Arrays/7.2-declaring-and-using-arrays/main.cpp
+++ 7-Arrays/7.2-declaring-and-using-arrays/main.cpp
@@ -1,32 +1,19 @@
 // Chapter 7: Arrays
-// ! 7.1 Arrays Introduction
+// ! 7.2 Declaring and using Arrays
 // ? |Lesson Start           | 08H : 48Min : 45 Sec |
-//*  |Code Start             | 00H : 00Min : 00 Sec |
+//*  |Code Start             | 08H : 59Min : 01 Sec |
 //*  |Next Lesson            | 00H : 00Min : 00 Sec |
 // >>: | g++ -std=c++20 -o build.gcc main.cpp |
 // ||: | clang++ -std=c++20 -o build.clang main.cpp |
 // del 
 /*
-# Chapter 7: Arrays
-## 7.1 Arrays Introduction
-
-||Time (00H : 00Min : 00 Sec)|
-|-|-|
- |Lesson Start           | 08H : 48Min : 45 Sec |  
- |Code Start             | 00H : 00Min : 00 Sec |  
- |Next Lesson            | 00H : 00Min : 00 Sec | 
-* [main.cpp](./main.cpp)
-* [Home](/README.md)
-* [pdf]()
-
----
 
 */
 #include<iostream>
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<" 7.1 Arrays Introduction "<<std::endl;
+    std::cout<<" 7.2 Declaring and using Arrays "<<std::endl;
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
 
     
