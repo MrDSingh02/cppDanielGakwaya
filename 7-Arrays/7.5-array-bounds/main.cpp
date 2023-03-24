549ace558b8bf14c2512ee129eec9ed1f9671707
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-03-24 12:46:21 +0530

    chapter 7 Array finished

--- 7-Arrays/7.5-array-bounds/main.cpp
+++ 7-Arrays/7.5-array-bounds/main.cpp
@@ -2,7 +2,7 @@
 // ! 7.5 Bounds of an Array
 // ? |Lesson Start           | 09H : 46Min : 46 Sec |
 //*  |Code Start             | 09H : 49Min : 21 Sec |
-//*  |Next Lesson            | 00H : 00Min : 00 Sec |
+//*  |Next Lesson            | 09H : 53Min : 26 Sec |
 // >>: | g++ -std=c++20 -o build.gcc main.cpp |
 // ||: | clang++ -std=c++20 -o build.clang main.cpp |
 // del 
@@ -15,6 +15,11 @@ int main(int argc, char const *argv[])
 {
     std::cout<<" 7.5 Bounds of an Array "<<std::endl;
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+
+    int number [] {1,2,3,4,5,6,7,8,9,0};
+
+    //>> read beyond bounds : will read garbage or crash your program
+    std::cout << "number[12] : " << number[12] << std::endl;
 
     
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
---
+++ 8-pointers/8.1-introduction/readme.md
@@ -0,0 +1,12 @@
+# Chapter 8: Pointers
+## 8.1 Introduction to Pointers
+||Time (00H : 00Min : 00 Sec)|
+|-|-|
+ |Lesson Start           | 09H : 53Min : 26 Sec |  
+ |Code Start             | 00H : 00Min : 00 Sec |  
+ |Next Lesson            | 00H : 00Min : 00 Sec | 
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+* [pdf]()
+
+---
