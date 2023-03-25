0bb202f0970cc4a9f46cb8cf0288d7ccb2babf11
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-03-25 08:44:21 +0530

    8.2 declaring and using pointers

---
+++ 8-pointers/8.2-declaring-&-using-pointers/main.cpp
@@ -0,0 +1,22 @@
+// Chapter 8: Pointers
+// ! 8.2 Declaring and Using Pointers
+// ? |Lesson Start           | 09H : 56Min : 05 Sec |
+//*  |Code Start             | 10H : 02Min : 37 Sec |
+//*  |Next Lesson            | 00H : 00Min : 00 Sec |
+// >>: | g++ -std=c++20 -o build.gcc main.cpp |
+// ||: | clang++ -std=c++20 -o build.clang main.cpp |
+// del 
+/*
+
+*/
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<" 8.2 Declaring and Using Pointers "<<std::endl;
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+
+    
+    std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+    return 0;
+}
---
+++ 8-pointers/8.2-declaring-&-using-pointers/readme.md
@@ -0,0 +1,12 @@
+# Chapter 8: Pointers
+## 8.2 Declaring and Using Pointers
+||Time (00H : 00Min : 00 Sec)|
+|-|-|
+ |Lesson Start           | 09H : 56Min : 05 Sec |  
+ |Code Start             | 10H : 02Min : 37 Sec |  
+ |Next Lesson            | 00H : 00Min : 00 Sec | 
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+* [pdf](./8.2-declaring%26-using-pointers.pdf)
+
+---
