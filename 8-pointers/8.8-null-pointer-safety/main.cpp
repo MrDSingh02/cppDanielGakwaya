7922e48c2165e16cbe6587845f2cf011da2b3ae2
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-04-02 21:48:49 +0530

    some

--- 8-pointers/8.8-null-pointer-safety/main.cpp
+++ 8-pointers/8.8-null-pointer-safety/main.cpp
@@ -1,17 +1,17 @@
 // Chapter 8: Pointers
-// ! 8.1 Introduction to Pointers
-// ? |Lesson Start           | 00H : 00Min : 00 Sec |
+// ! 8.8 Null Pointer Safety
+// ? |Lesson Start           | 11H : 38Min : 04 Sec |
 //-  |Code Start             | 00H : 00Min : 00 Sec |
 //-  |Next Lesson            | 00H : 00Min : 00 Sec |
 // >>: | g++ -std=c++20 -o build.gcc main.cpp |
-// /: | clang++ -std=c++20 -o build.clang main.cpp |
-/// del 
+/// ||: | clang++ -std=c++20 -o build.clang main.cpp |
+// del 
 /*
 # Chapter 8: Pointers
-## 8.1 Introduction to Pointers
+## 8.8 Null Pointer Safety
 ||Time (00H : 00Min : 00 Sec)|
 |-|-|
- |Lesson Start           | 00H : 00Min : 00 Sec |  
+ |Lesson Start           | 11H : 38Min : 04 Sec |  
  |Code Start             | 00H : 00Min : 00 Sec |  
  |Next Lesson            | 00H : 00Min : 00 Sec | 
 * [main.cpp](./main.cpp)
@@ -25,8 +25,10 @@
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<" 8.1 Introduction to Pointers "<<std::endl;
+    std::cout<<" 8.8 Null Pointer Safety "<<std::endl;
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
+
+    
 
     
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
