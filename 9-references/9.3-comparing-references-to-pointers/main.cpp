63ff39cdfdac84291eac5f267f056605b9455f0c
Dayanand Singh <110038166+DayanandSingh@users.noreply.github.com>
2023-04-21 02:26:59 +0530

    9.3 finish

--- 9-references/9.3-comparing-references-to-pointers/main.cpp
+++ 9-references/9.3-comparing-references-to-pointers/main.cpp
@@ -1,24 +1,12 @@
 // Chapter 9: References
 // ! 9.3 Comparing pointers and references
 // ? |Lesson Start           | 12H : 22Min : 29 Sec |
-//-  |Code Start             | 00H : 00Min : 00 Sec |
-//-  |Next Lesson            | 00H : 00Min : 00 Sec |
+//-  |Code Start             | 12H : 27Min : 33 Sec |
+//-  |Next Lesson            | 12H : 37Min : 26 Sec |
 // >>: | g++ -std=c++20 -o build.gcc main.cpp |
 // /: | clang++ -std=c++20 -o build.clang main.cpp |
 /// del 
 /*
-# Chapter 9: References
-## 9.3 Comparing pointers and references
-||Time (00H : 00Min : 00 Sec)|
-|-|-|
- |Lesson Start           | 12H : 22Min : 29 Sec |  
- |Code Start             | 00H : 00Min : 00 Sec |  
- |Next Lesson            | 00H : 00Min : 00 Sec | 
-* [main.cpp](./main.cpp)
-* [Home](/README.md)
-* [pdf]()
-
----
 
 */
 #include<iostream>
@@ -28,7 +16,78 @@ int main(int argc, char const *argv[])
     std::cout<<" 9.3 Comparing pointers and references "<<std::endl;
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
 
-    
+    //? Declare pointer and reference
+
+    double double_value {12.34};
+    double& ref_double_value {double_value};/// Reference to double_value
+    double* p_double_value {&double_value}; /// Pointer to double_value
+
+    /// Reading
+    std::cout << "double_value : " << double_value << std::endl;
+    std::cout << "ref_double_value : " << ref_double_value << std::endl;
+    std::cout << "p_double_value : " << p_double_value << std::endl;
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;
+
+    //> Writting through pointer
+    *p_double_value = 15.44;
+
+    std::cout << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;
+    std::cout << "ref_double_value : " << ref_double_value << std::endl;
+    std::cout << "p_double_value : " << p_double_value << std::endl;
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;
+
+    //> Writting through Reference
+    ref_double_value = 18.44;
+
+    std::cout << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;
+    std::cout << "ref_double_value : " << ref_double_value << std::endl;
+    std::cout << "p_double_value : " << p_double_value << std::endl;
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;
+
+    //> Make the reference reference something else
+    double some_other_double {78.45};
+
+    ref_double_value = some_other_double;
+
+    std::cout << "Make the reference reference something else" << std::endl;
+    std::cout << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;
+    std::cout << "ref_double_value : " << ref_double_value << std::endl;
+    std::cout << "p_double_value : " << p_double_value << std::endl;
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;
+
+    //> Make the Pointer point to something else
+    p_double_value = &some_other_double;
+
+    std::cout << "Make the Pointer point to something else" << std::endl;
+    std::cout << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;
+    std::cout << "ref_double_value : " << ref_double_value << std::endl;
+    std::cout << "&double_value : " << &double_value << std::endl;
+    std::cout << "&ref_double_value : " << &ref_double_value << std::endl;
+    std::cout << "p_double_value : " << p_double_value << std::endl;
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;
+
+    *p_double_value = 555.5;
+
+    std::cout << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;
+    std::cout << "ref_double_value : " << ref_double_value << std::endl;
+    std::cout << "&double_value : " << &double_value << std::endl;
+    std::cout << "&ref_double_value : " << &ref_double_value << std::endl;
+    std::cout << "p_double_value : " << p_double_value << std::endl;
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;
+
+
+    //! Error
+    // int& ref_some{};
+    /* initial value of reference to non-const must be an lvalue */
+
+
+
+
     std::cout<<" == == == == == == == == == == == == == == == == == == == == == == == == == == == == == "<<std::endl;
     return 0;
 }
---
+++ 9-references/9.3-comparing-references-to-pointers/readme.md
@@ -0,0 +1,12 @@
+# Chapter 9: References
+## 9.3 Comparing pointers and references
+||Time (00H : 00Min : 00 Sec)|
+|-|-|
+ |Lesson Start           | 12H : 22Min : 29 Sec |  
+ |Code Start             | 12H : 27Min : 33 Sec |  
+ |Next Lesson            | 12H : 37Min : 26 Sec | 
+* [main.cpp](./main.cpp)
+* [Home](/README.md)
+* [pdf](9.3-comparing-pointer-%26-references.pdf)
+
+---
